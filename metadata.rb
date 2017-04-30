f63adef305820a8bf7f4d28d12761c98b39f28f0
Richard <richard.wigley@gmail.com>
2017-04-30 18:05:26 +0100

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'bcs_ruby'
-maintainer       'Richard Wigley '
+maintainer       'BCS Ltd'
 maintainer_email 'richard.wigley@github'
 license          'MIT'
 description      'Installs/Configures bcs_ruby'
@@ -9,6 +9,8 @@ version          '2.0.0'
 source_url       'git@github.com:BCS-io-provision/bcs_ruby' if respond_to?(:source_url)
 issues_url       'git@github.com:BCS-io-provision/bcs_ruby/issues' if respond_to?(:issues_url)
 
-supports 'ubuntu', '>= 14.04'
+chef_version     '>= 12.5' if respond_to?(:chef_version)
+
+supports         'ubuntu', '>= 14.04'
 
 depends 'brightbox-ruby'
